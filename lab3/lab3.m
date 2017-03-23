fb943047053e969e15c54dacb02d9835233391db
eudaemonal <eudaemonal@gmail.com>
2017-03-23 16:27:42 +1100

    udpate

--- lab3/lab3.m
+++ lab3/lab3.m
@@ -1,4 +1,12 @@
+% EE9723 Lab 3
 
 
-load('../src/sample1.wav');
+[sample,Fs] = audioread('../Speech samples/sample1.wav');
 
+% frame_fft(sample, 1600,256)
+frame = sample(700:3600)
+
+
+y = acf(frame,2000)
+plot(y)
+peakfinder(y)
